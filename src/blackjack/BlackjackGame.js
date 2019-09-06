a6e7e5d93adc4d8d07002aabfbd599d01ddb39d3
Simon Ayzman <simonayzman@gmail.com>
2019-09-06 13:44:28 -0500

    Added shell for blackjack game

---
+++ src/blackjack/BlackjackGame.js
@@ -0,0 +1,57 @@
+const inquirer = require('inquirer');
+const chalk = require('chalk');
+const clear = require('clear');
+const figlet = require('figlet');
+
+const Deck = require("cards/Deck");
+const BlackjackPlayer = require("blackjack/BlackjackPlayer");
+
+class BlackjackGame {
+    constructor(numberOfPlayers) {
+        this.deck = new Deck;
+        this.dealer = new BlackjackPlayer("DEALER");
+        this.players = [];
+        for (let playerIndex = 0; playerIndex < numberOfPlayers; playerIndex++) {
+            const player = new BlackjackPlayer(`Player ${playerIndex+1}`);
+            this.players.push(player);
+        }
+    }
+
+    async start() {
+        const introText = figlet.textSync("Let's play Blackjack!", { horizontalLayout: 'full' });
+        console.log(chalk.redBright(introText));
+
+        let stillPlaying = true;
+        while (stillPlaying) {
+            this.playRound();
+            const { continuePlaying } = await inquirer.prompt([
+                {
+                    name: 'continuePlaying',
+                    type: 'confirm',
+                    message: 'Do you want to continue playing?',
+                },
+            ]);
+            stillPlaying = continuePlaying;
+        }
+        const exitText = figlet.textSync("Thanks for playing!", { horizontalLayout: 'full' });
+        console.log(chalk.redBright(exitText));
+    }
+
+    resetRound() {
+        this.deck.reset();
+        this.deck.shuffle();
+
+        for (let playerIndex = 0; playerIndex < this.players.length; playerIndex++) {
+            const player = this.players[playerIndex];
+            player.resetHand();
+        }
+
+        clear();
+    }
+
+    playRound() {
+        this.resetRound();
+    }
+}
+
+module.exports = BlackjackGame;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const program = require('commander');
 const chalk = require('chalk');
 const figlet = require('figlet');
 
+const BlackjackGame = require("blackjack/BlackjackGame");
+
 const DEFAULT_NUMBER_OF_PLAYERS = 1;
 
 program.version('1.0.0');
@@ -14,4 +16,6 @@ if (Number.isNaN(numberOfPlayers) === true) {
     console.log("Defaulting game to 1 player.");
     numberOfPlayers = DEFAULT_NUMBER_OF_PLAYERS;
 }
+
+const game = new BlackjackGame(numberOfPlayers);
 game.start();
