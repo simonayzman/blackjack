217caa8356061f13bbda217258190b5aed7e337d
Simon Ayzman <simonayzman@gmail.com>
2019-09-06 17:13:59 -0500

    Added basic hand value tests

--- test/BlackjackPlayer.js
+++ test/BlackjackPlayer.js
@@ -4,16 +4,56 @@ const Card = require('cards/Card');
 const BlackjackPlayer = require('blackjack/BlackjackPlayer');
 const constants = require('lib/constants');
 
-const { SUITS, RANKS } = constants;
+const { RANKS, SUITS } = constants;
 
 describe('BlackjackPlayer', () => {
     describe('getHandValue()', () => {
-        it('Empty hand = 0', () => {
-            const player = new BlackjackPlayer("TestPlayer");
-            
-            const handValue = player.getHandValue();
-            const expectedHandValue = 0;
-            assert.equal(handValue, expectedHandValue);
+        describe('No cards', () => {
+            it('Empty hand = 0', () => {
+                const player = new BlackjackPlayer("TestPlayer");
+                
+                const handValue = player.getHandValue();
+                const expectedHandValue = 0;
+                assert.equal(handValue, expectedHandValue);
+            });
+        });
+
+        describe('One card', () => {
+            it('5♥ = 5', () => {
+                const player = new BlackjackPlayer("TestPlayer");
+                player.receiveNewCard(new Card(RANKS.Five, SUITS.Hearts));
+
+                const handValue = player.getHandValue();
+                const expectedHandValue = 5;
+                assert.equal(handValue, expectedHandValue);
+            });
+
+            it('10♥ = 10', () => {
+                const player = new BlackjackPlayer("TestPlayer");
+                player.receiveNewCard(new Card(RANKS.Ten, SUITS.Hearts));
+
+                const handValue = player.getHandValue();
+                const expectedHandValue = 10;
+                assert.equal(handValue, expectedHandValue);
+            });
+
+            it('J♥ = 10', () => {
+                const player = new BlackjackPlayer("TestPlayer");
+                player.receiveNewCard(new Card(RANKS.Jack, SUITS.Hearts));
+
+                const handValue = player.getHandValue();
+                const expectedHandValue = 10;
+                assert.equal(handValue, expectedHandValue);
+            });
+
+            it('A♥ = 11', () => {
+                const player = new BlackjackPlayer("TestPlayer");
+                player.receiveNewCard(new Card(RANKS.Ace, SUITS.Hearts));
+
+                const handValue = player.getHandValue();
+                const expectedHandValue = 11;
+                assert.equal(handValue, expectedHandValue);
+            });
         });
     });
 });
