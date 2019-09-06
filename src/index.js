87a9064a617301af01ec8cd997cef8206ebfe973
Simon Ayzman <simonayzman@gmail.com>
2019-09-06 17:13:45 -0500

    Updated program entrypoint with prompting vs. args

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const inquirer = require('inquirer');
 const program = require('commander');
 const chalk = require('chalk');
 const figlet = require('figlet');
@@ -8,14 +9,21 @@ const DEFAULT_NUMBER_OF_PLAYERS = 1;
 
 program.version('1.0.0');
 program.description('Blackjack game for the Kleiner Perkins Fellowship 2019/20, by Simon Ayzman.');
-program.option("-p, --players <number>", "How many players to play with?");
+program.option("-p, --players <number>", "Number of players, not including the dealer. (Min: 1, Max: 6)", DEFAULT_NUMBER_OF_PLAYERS);
 program.parse(process.argv);
 
-let numberOfPlayers = parseInt(program.players);
-if (Number.isNaN(numberOfPlayers) === true) {
-    console.log("Defaulting game to 1 player.");
-    numberOfPlayers = DEFAULT_NUMBER_OF_PLAYERS;
+async function startBlackjackGame() {
+    const { numberOfPlayers } = await inquirer.prompt([
+        {
+            name: 'numberOfPlayers',
+            type: 'list',
+            message: `How many players (not including the dealer)? Minimum 1. Maximum: 6.`,
+            choices: [1,2,3,4,5,6],
+        },
+    ]);
+
+    const game = new BlackjackGame(numberOfPlayers);
+    game.start();
 }
 
-const game = new BlackjackGame(numberOfPlayers);
-game.start();
+startBlackjackGame();
