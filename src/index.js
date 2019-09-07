15dcd5aded6b18980c6d579c3e21644f2b6bce0c
Simon Ayzman <simonayzman@gmail.com>
2019-09-06 21:44:40 -0500

    Removed use of commander dependency

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 const inquirer = require('inquirer');
-const program = require('commander');
 const chalk = require('chalk');
 const figlet = require('figlet');
 
 const BlackjackGame = require("blackjack/BlackjackGame");
-
-const DEFAULT_NUMBER_OF_PLAYERS = 1;
-
-program.version('1.0.0');
-program.description('Blackjack game for the Kleiner Perkins Fellowship 2019/20, by Simon Ayzman.');
-program.option("-p, --players <number>", "Number of players, not including the dealer. (Min: 1, Max: 6)", DEFAULT_NUMBER_OF_PLAYERS);
-program.parse(process.argv);
 
 async function startBlackjackGame() {
     const { numberOfPlayers } = await inquirer.prompt([
